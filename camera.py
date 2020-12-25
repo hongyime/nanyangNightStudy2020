cb9ba86bdcf71e7777139da9db99f003dd766507
bryanseah234 <bryanseah234@gmail.com>
2020-12-25 19:33:31 +0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -37,26 +37,22 @@ class VideoCamera(object):
 
         while True:
             success, frame = self.video.read()
-            try:
-                for i in decode(frame):
-                    qrcode = i.data.decode('utf-8')
-                    print(qrcode)
-                    if qrcode in qrcodes:
-                        verification = 'VALID QR'
-                        myColor = (0,255,0)
-                    else:
-                        verification = 'INVALID QR'
-                        myColor = (0, 0, 255)
+            for i in decode(frame):
+                qrcode = i.data.decode('utf-8')
+                print(qrcode)
+                if qrcode in qrcodes:
+                    verification = 'VALID QR'
+                    myColor = (0,255,0)
+                else:
+                    verification = 'INVALID QR'
+                    myColor = (0, 0, 255)
 
-                    pts = np.array([i.polygon],np.int32)
-                    pts = pts.reshape((-1,1,2))
-                    cv2.polylines(frame,[pts],True,myColor,3)
-                    pts2 = i.rect
-                    cv2.putText(frame,verification,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX,1,myColor,2)    
+                pts = np.array([i.polygon],np.int32)
+                pts = pts.reshape((-1,1,2))
+                cv2.polylines(frame,[pts],True,myColor,3)
+                pts2 = i.rect
+                cv2.putText(frame,verification,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX,1,myColor,2)    
 
-                # encode OpenCV raw frame to jpg and displaying it
-                ret, jpeg = cv2.imencode('.jpg', frame)
-                return jpeg.tobytes()
-            except:
-                error = sys.exc_info()[0]
-                return render_template("scan.html", error=error) 
+            # encode OpenCV raw frame to jpg and displaying it
+            ret, jpeg = cv2.imencode('.jpg', frame)
+            return jpeg.tobytes()
