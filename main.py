52e45b6bca2142a5b9099cec133ca142b8139432
Bryan <hongyime@users.noreply.github.com>
2021-01-16 15:29:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -96,13 +96,35 @@ def decode_qr(uploaded_file):
     clean_data = str(full_data, 'utf-8')
     return clean_data
 
+def checkRecaptcha(response, secretkey):
+        url = 'https://www.google.com/recaptcha/api/siteverify?'
+        url = url + 'secret=' + str(secretkey)
+        url = url + '&response=' +str(response)
+
+        jsonobj = json.loads(urllib2.urlopen(url).read())
+        print jsonobj['success']
+        if jsonobj['success']:
+            print jsonobj['success']
+            return True
+        else:
+            return False
+
 ''' START OF APP '''
 
-app = Flask(__name__, template_folder='templates') 
+app = Flask(__name__, template_folder='templates')
+
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['MAX_CONTENT_LENGTH'] = 2048 * 2048
 app.config['UPLOAD_EXTENSIONS'] = ['.png', ".PNG", '.JPEG', '.JPG', ".jpeg", ".jpg"]
 correct_user, correct_pass, start, login, limit = admin_details()
+
+app.config['RECAPTCHA_USE_SSL'] = False
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LeKpS4aAAAAABrXNs2o5Adx9YGG8RCicK4_sgva'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LeKpS4aAAAAAM_vT-5MLZMBYaNAV8w9TuQUlsbm'
+app.config['RECAPTCHA_OPTIONS'] = {'theme':'black'}
+
+RECAPTCHA_PUBLIC_KEY = '6LeKpS4aAAAAABrXNs2o5Adx9YGG8RCicK4_sgva'
+RECAPTCHA_PRIVATE_KEY = '6LeKpS4aAAAAAM_vT-5MLZMBYaNAV8w9TuQUlsbm'
 
 @app.after_request
 def add_header(r):
@@ -156,38 +178,47 @@ def display():
 
 @app.route('/login', methods=['POST','GET'])
 def login():
+    
     correct_user, correct_pass, start, login, limit = admin_details()
     login = 'False'
     with open('static/login.txt', 'w') as f:
         f.write("username:password:start:login:limit")
         f.write('\n')
         f.write(f"{correct_user}:{correct_pass}:{start}:{login}:{limit}")
+    
     if request.method == "POST":
-        try:
-            input_user = request.form['username']
-            b_input_user = bytes(str(input_user), 'utf-8') #change to byte
-            hash_input_user = hashlib.sha512(b_input_user).hexdigest()
+        response = request.form.get('g-recaptcha-response')
+        if checkRecaptcha(response, RECAPTCHA_PRIVATE_KEY):
+            try:
+                input_user = request.form['username']
+                b_input_user = bytes(str(input_user), 'utf-8') #change to byte
+                hash_input_user = hashlib.sha512(b_input_user).hexdigest()
+
+                input_pass = request.form['password']
+                b_input_pass = bytes(str(input_pass), 'utf-8') #change to byte
+                hash_input_pass = hashlib.sha512(b_input_pass).hexdigest()
             
-            input_pass = request.form['password']
-            b_input_pass = bytes(str(input_pass), 'utf-8') #change to byte
-            hash_input_pass = hashlib.sha512(b_input_pass).hexdigest()
-            
-            if hash_input_user == correct_user and hash_input_pass == correct_pass:
-                with open('static/login.txt', 'w') as f:
-                    f.write("username:password:start:login:limit")
-                    f.write('\n')
-                    login = "True"
-                    print(correct_pass)
-                    print(correct_user)
-                    f.write(f"{correct_user}:{correct_pass}:{start}:{login}:{limit}")
-                return redirect(url_for("admin"))
-            else:
+                if hash_input_user == correct_user and hash_input_pass == correct_pass:
+                    with open('static/login.txt', 'w') as f:
+                        f.write("username:password:start:login:limit")
+                        f.write('\n')
+                        login = "True"
+                        print(correct_pass)
+                        print(correct_user)
+                        f.write(f"{correct_user}:{correct_pass}:{start}:{login}:{limit}")
+                    return redirect(url_for("admin"))
+                else:
+                    error = "Invalid credentials."
+                    return render_template("login.html", error=error)
+            except:
                 error = "Invalid credentials."
                 return render_template("login.html", error=error)
-        except:
-            error = "Invalid credentials."
+
+        else:
+            error = "Captcha failed."
             return render_template("login.html", error=error)
-    else:
+        
+    else: #if request is not post
         return render_template("login.html")
 
 @app.route('/admin', methods=['POST','GET'])
